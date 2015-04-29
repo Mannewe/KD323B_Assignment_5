45c2fd9f782164c7b789344ced3595b28373d5a2
hansLindberg <hansperkrister.lindberg@gmail.com>
2015-04-29 23:34:12 +0200

    Initial commit in attempt to change computer

---
+++ app/src/main/java/se/k3/antonochisak/kd323bassignment5/adapters/TrendingMoviesAdapter.java
@@ -0,0 +1,22 @@
+package se.k3.antonochisak.kd323bassignment5.adapters;
+
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+import se.k3.antonochisak.kd323bassignment5.models.movie.Movie;
+
+/**
+ * Created by Hans on 2015-04-29.
+ */
+public class TrendingMoviesAdapter extends BaseAdapter{
+
+    ArrayList<Movie> mMovies;
+    LayoutInflater mLayoutInflater;
+
+
+    public TrendingMoviesAdapter(ArrayList<Movie> mMovies) {
+        this.mMovies = mMovies;
+    }
+}
